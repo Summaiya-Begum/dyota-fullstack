a4389d0482e0476c19c31c9e992f1fd6435ae1a4
Summaiya <gzlfathima786@gmail.com>
2023-05-21 19:12:55 +0530

    Fix: title height

--- client/src/components/Dyota.tsx
+++ client/src/components/Dyota.tsx
@@ -84,7 +84,7 @@ export const Dyota = () => {
 
       {
         isLoading ? (
-          <div className="relative h-[100svh]">
+          <div className="relative min-h-[100svh] h-[100svh]">
             <div className="flex items-center justify-center h-[92vh] absolute w-full">
               <div className="grid grid-cols-2 ">
                 {motionDivs.map((motionDiv, index) => (
@@ -125,7 +125,7 @@ export const Dyota = () => {
           : (
             <div className="h-full">
               <div className=''>
-                <img src="https://img.freepik.com/free-psd/fashion-banner-design-template_23-2149072746.jpg?w=1060&t=st=1660568902~exp=1660569502~hmac=39e55eeccd764da667826fb179a83442ba0935b0213d5880f0c72f2199c3d3c1" alt="banner" className="m-auto w-[100%] h-[80svh]" />
+                <img src="https://img.freepik.com/free-psd/fashion-banner-design-template_23-2149072746.jpg?w=1060&t=st=1660568902~exp=1660569502~hmac=39e55eeccd764da667826fb179a83442ba0935b0213d5880f0c72f2199c3d3c1" alt="banner" className="hidden md:block m-auto w-[100%] h-[80svh]" />
               </div>
               <div className='flex gap-4 m-auto justify-center items-center mt-10 mb-10'>
                 <button className='px-4 py-1 bg-[#3BD188] text-black font-semibold text-lg hover:bg-black hover:text-[#3BD188]' onClick={() => Filter("low")}>Low To High</button>
@@ -133,10 +133,10 @@ export const Dyota = () => {
               </div>
 
 
-              <div 
-              className=" w-[100%] h-auto py-16 px-20  m-auto gap-5  overflow-hidden md:grid md:grid-cols-2 md:gap-10  md:m-auto  md:justify-center md:overflow-visible lg:grid-cols-3 "
+              <div
+                className=" w-[100%] h-auto px-10 py-20 md:py-16 md:px-20  m-auto gap-10  overflow-hidden md:grid md:grid-cols-2 md:gap-10  md:m-auto  md:justify-center md:overflow-visible lg:grid-cols-3 "
               >
-                {productData?.map((el: any , i:any) => (
+                {productData?.map((el: any, i: any) => (
                   <motion.div
                     key={el.id}
                     className="px-8 py-8 shadow-lg shadow-green-500/50 hover:shadow-lg hover:shadow-green-800/80 font-medium rounded-xl cursor-pointer"
@@ -145,12 +145,29 @@ export const Dyota = () => {
                     viewport={{ once: true, amount: 0.3 }}
                     transition={{ duration: 0.3, delay: i * 0.1 }}
                   >
-                    <img src={el.image} alt={el.title} className="h-60 w-60 m-auto"/>
-                    <p className="text-lg font-semibold mt-2">Price: <span className="text-green-500 text-[1.2vw] font-normal ml-5"> ${el.price}</span></p>
-                    <p className="text-lg font-semibold">Rating: <span className=" text-[1.2vw] font-normal ml-2">{el.rating.rate}</span></p>
-                    <p className="text-lg font-semibold">Category: <span className=" text-[1.2vw] font-normal">{el.category}</span></p>
-                    <p className="text-lg font-semibold h-[10vh]" >Title: <span className=" text-[1.2vw] font-normal"> {el.title}</span></p>
-                    <div className="flex items-center justify-center mt-6 bg-[#3BD188] text-black font-semibold text-lg hover:bg-black hover:text-[#3BD188] rounded-2xl">
+                    <img src={el.image} alt={el.title} className="h-60 w-60 m-auto" />
+                    <p className="text-lg font-semibold mt-2">Price:
+                      <span className="text-green-500 text-md lg:text-[1.2vw] font-normal ml-5">
+                        ${el.price}
+                      </span>
+                    </p>
+                    <p className="text-lg font-semibold">Rating:
+                      <span className=" text-md lg:text-[1.2vw] font-normal ml-2">
+                        {el.rating.rate}
+                      </span>
+                    </p>
+                    <p className="text-lg font-semibold">Category:
+                      <span className="text-md lg:text-[1.2vw] font-normal ml-2">
+                        {el.category}
+                      </span>
+                    </p>
+                    <p className="text-lg font-semibold h-[10vh]" >
+                      Title: 
+                      <span className="text-md lg:text-[1.2vw] font-normal ml-2">
+                        {el.title}
+                      </span>
+                    </p>
+                    <div className="flex w-[50vw] md:w-full items-center m-auto justify-center mt-6 bg-[#3BD188] text-black font-semibold text-lg hover:bg-black hover:text-[#3BD188] rounded-2xl">
                       <button className="px-4 py-2 ">Show Details</button>
                     </div>
                   </motion.div>
